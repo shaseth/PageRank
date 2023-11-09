f9b5aa7a4baf3b983cebd1ddadaebc8945888232
shaseth <131016800+shaseth@users.noreply.github.com>
2023-11-09 08:49:19 -0600

    Update pageRank.java

--- pageRank.java
+++ pageRank.java
@@ -6,10 +6,19 @@
 public class PageRank {
 
   public static void pageRank() {
-
+    double rank;
+    int[] outPages;
+    double newRank;
   }
 
   public static void main (String[] args) {
-
+    
   }
 }
+
+// for each node:
+// divide rank + increase newRank for each outgoing link
+// set self.newRank to 0
+// foor each node:
+// rank = newRank
+
