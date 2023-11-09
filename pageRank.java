9224ea41180f51d2b607ba5b1ea1fba67cb20db4
shaseth <131016800+shaseth@users.noreply.github.com>
2023-11-09 08:35:21 -0600

    commit

--- pageRank.java
+++ pageRank.java
@@ -1,1 +1,6 @@
+// start with initial values, do pagerank - keep updated values but don't update yet
+// at end damping factor, then update scores
+// 0.002
+// 100 iterations first, see if it works then keep going
 
+// error
