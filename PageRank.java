7b6e564bfc9d6fb4376096fdaeaf615e53ac246b
entropicCowboy <118836121+entropicCowboy@users.noreply.github.com>
2023-11-10 10:35:13 -0600

    Update PageRank.java
    Fixed errors

--- PageRank.java
+++ PageRank.java
@@ -1,39 +1,38 @@
-// start with initial values, do pagerank - keep updated values but don't update yet
-// at end damping factor, then update scores
-// 0.002
-// 100 iterations first, see if it works then keep going
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PageRank {
-  Scanner scanner = new Scanner(System.in);
-  ArrayList<Page> pages = new ArrayList<>;
-  int pageCount = 0;
-  
-  // read in values and create page instances
-  while (scanner.hasNextLine == True) {
-    String line = scanner.nextLine();
-    String[] spLine = line.split(",");
-    String p1 = spLine[0];
-    String p2 = spLine[2];
-    Page p1 = new Page();
-    Page p2 = new Page();
-  
-    // don't even have to check if its directed or not because 
-    // both always lists the outgoing page first
-    p1.outPages.add(p2);
-    p1.numOutPages++;
-    pages.add(p1);
-    pageCount++;
-  } // end while loop
 
+  // start with initial values, do pagerank - keep updated values but don't update yet
+  // at end damping factor, then update scores
+  // 0.002
+  // 100 iterations first, see if it works then keep going
   public static void rank() {
 
   } // end rank()
 
   public static void main (String[] args) {
-    
-  }
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Page> pages = new ArrayList<>();
+    int pageCount = 0;
+  
+    // read in values and create page instances
+    while (scanner.hasNextLine() == true) {
+      String line = scanner.nextLine();
+      String[] spLine = line.split(",");
+      String s1 = spLine[0];
+      String s2 = spLine[2];
+      Page p1 = new Page(s1);
+      Page p2 = new Page(s2);
+  
+      // don't even have to check if its directed or not because 
+      // both always lists the outgoing page first
+      p1.outPages.add(p2);
+      p1.numOutPages++;
+      pages.add(p1);
+      pageCount++;
+    } // end while loop
+  } // end main
 } // end PageRank class
 
 // for each node:
