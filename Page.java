8ff22c18fa546764bf8bcc97b32a9baa9fca2b06
entropicCowboy <118836121+entropicCowboy@users.noreply.github.com>
2023-11-10 10:28:48 -0600

    Update Page.java
    added id

--- Page.java
+++ Page.java
@@ -1,12 +1,14 @@
 // Creates Page class for pageRank
 public class Page {
+  String id;
   double rank;
   ArrayList<Integer> outPages;
   int numOutPages;
   double newRank;
   
   // constructor
-  public Page () {
+  public Page (String id) {
+    this.id = id;
     this.rank = 0;
     this.outPages = new ArrayList<>();
     this.numOutPages = 0;
