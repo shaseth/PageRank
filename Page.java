4c800da8b270d4fc6229c1326c14d822c29db0b8
entropicCowboy <118836121+entropicCowboy@users.noreply.github.com>
2023-11-10 09:02:52 -0600

    Update Page.java

--- Page.java
+++ Page.java
@@ -1,1 +1,15 @@
 // Creates Page class for pageRank
+public class Page {
+  double rank;
+  ArrayList<Integer> outPages;
+  int numOutPages;
+  double newRank;
+  
+  // constructor
+  public Page () {
+    this.rank = 0;
+    this.outPages = new ArrayList<>();
+    this.numOutPages = 0;
+    this.newRank = 0;
+  } // end constructor
+} // end Page class
