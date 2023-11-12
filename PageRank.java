9aae34fd2a5f2469a8940c84ae36760eac33fa61
entropicCowboy <118836121+entropicCowboy@users.noreply.github.com>
2023-11-12 11:00:52 -0600

    Update PageRank.java
    fixed an error with creating new page instances for the same page

--- PageRank.java
+++ PageRank.java
@@ -14,7 +14,10 @@ public class PageRank {
   public static void main (String[] args) {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Page> pages = new ArrayList<>();
+    // keeps track of individual pages
+    HashMap<String, Page> idPageDict = new HashMap<>();
     int pageCount = 0;
+    int iterations = 100;
   
     // read in values and create page instances
     while (scanner.hasNextLine() == true) {
@@ -24,13 +27,32 @@ public class PageRank {
       String s2 = spLine[2];
       Page p1 = new Page(s1);
       Page p2 = new Page(s2);
+
+      // check if pages already exist, if not add them
+      if (idPageDict.containsKey(s1)) {
+        p1 = idPageDict.get(s1);
+	   	}
+	    else { // adding p1
+		    p1 = new Page(s1);
+		    pages.add(p1);
+		    pageCount++;
+		    idPageDict.put(s1,p1);
+	    }
+	    
+	    if (idPageDict.containsKey(s2)) {
+    		p2 = idPageDict.get(s2);
+	    }
+	    else { // adding p2
+		    p2 = new Page(s2);
+		    pages.add(p2);
+		    pageCount++;
+		    idPageDict.put(s2,p2);
+	    }
   
       // don't even have to check if its directed or not because 
       // both always lists the outgoing page first
       p1.outPages.add(p2);
       p1.numOutPages++;
-      pages.add(p1);
-      pageCount++;
     } // end while loop
   } // end main
 } // end PageRank class
